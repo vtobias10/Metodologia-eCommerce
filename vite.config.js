65685b25c9340430caff1ea5eae5cb9f7bf33e3f
vtobias10 <145521414+vtobias10@users.noreply.github.com>
2024-11-25 18:52:43 -0300

    commit para vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Configura la base URL para una aplicación de enrutamiento de React
+  base: '/',
+  build: {
+    outDir: 'dist',  // Directorio donde se almacenan los archivos construidos
+  },
 })
